583a454f1a08242a782833c9fc252be1c0f0b9a3
cholee <cholee@hrd.net>
2021-04-28 15:48:09 +0900

    j

---
+++ webProg/src/ajax/ReturnServlet.java
@@ -0,0 +1,29 @@
+package ajax;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/javascript/return.jsp")
+public class ReturnServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ReturnServlet() {
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String val = request.getParameter("username");
+		response.getWriter().print("{\"username\":\"" + val + "\"}");
+	}
+
+}
